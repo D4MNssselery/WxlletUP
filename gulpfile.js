4485df16bcb58e3f85698366ceec0475450b8601
D4MNssselery <thatswhyiamsotired@mail.ru>
2023-08-30 19:12:24 +0300

    next commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,6 @@
-const browserSync = require('browser-sync')
 let gulp = require('gulp'),
     sass = require('gulp-sass'),
     rename = require('gulp-rename'),
-    autoPrefixer = require('gulp-autoprefixer'),
-    GulpUglify = require('gulp-uglify'),
     browserSync = require('browser-sync'),
     autoprefixer = require('gulp-autoprefixer'),
     concat = require('gulp-concat'),
@@ -12,7 +9,7 @@ let gulp = require('gulp'),
     cssmin = require('gulp-cssmin');
 
   gulp.task('clean', async function () {
-    del.deleteSync('build')
+    del.sync('build')
   });
 
   gulp.task('sass', function () {
@@ -28,19 +25,19 @@ let gulp = require('gulp'),
 
 gulp.task('style', function() {
   return gulp.src([
-    "node_modules/normalize.css/normalize.css", "node_modules/slick-carousel/slick/slick.css","node_modules/magnific-popup/dist/magnific-popup.css","node_modules/jquery-form-styler/dist/jquery.formstyler.css","node_modules/jquery-form-styler/dist/jquery.formstyler.theme.css"
+    'node_modules/normalize.css/normalize.css', 'node_modules/slick-carousel/slick/slick.css','node_modules/magnific-popup/dist/magnific-popup.css','node_modules/jquery-form-styler/dist/jquery.formstyler.css','node_modules/jquery-form-styler/dist/jquery.formstyler.theme.css'
   ])
     .pipe(concat('libs.min.css'))
     .pipe(cssmin())
-  .pipe(gulp.dest('WxlletUP/css'));
+  .pipe(gulp.dest('WxlletUP/css'))
 });
 
 gulp.task('script', function (){
-  return gulp.src(["node_modules/slick-carousel/slick/slick.js","node_modules/magnific-popup/dist/magnific-popup.js","node_modules/jquery-form-styler/dist/jquery.formstyler.js",
+  return gulp.src(['node_modules/slick-carousel/slick/slick.js','node_modules/magnific-popup/dist/jquery.magnific-popup.js','node_modules/jquery-form-styler/dist/jquery.formstyler.js',
   ])
     .pipe(concat('libs.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('app/js'))
+    .pipe(gulp.dest('WxlletUP/js'))
 });
 
 gulp.task('html', function() {
@@ -63,19 +60,23 @@ gulp.task('browser-sync', function () {
 
 gulp.task('export', async function () {
   let buildHtml = gulp.src('WxlletUP/**/*.html')
-    .pipe(gulp.dest(build));
+    .pipe(gulp.dest('build'));
 
-  let buildCss = gulp.src("WxlletUP/css/*.css").pipe(gulp.dest(build/css));
+  let BuildCss = gulp.src("WxlletUP/css/**/*.css").pipe(gulp.dest('build/css'));
 
-  let buildJs = gulp.src("WxlletUP/js/**/*.js").pipe(gulp.dest(build/  js));
+  let BuildJs = gulp.src("WxlletUP/js/**/*.js").pipe(gulp.dest('build/js'));
 
-  let buildFonts = gulp.src("WxlletUP/fonts/**/*.*").pipe(gulp.dest(build/   fonts));
+  let BuildFonts = gulp.src("WxlletUP/fonts/**/*.*").pipe(gulp.dest('build/fonts'));
 
-  let buildImg = gulp.src("WxlletUP/images/**/*.*").pipe(gulp.dest(build/ images));
+  let BuildImg = gulp.src("WxlletUP/images/**/*.*").pipe(gulp.dest('build/images'));
 });
 
 gulp.task('watch', function() {
-  gulp.watch('WxlletUP/scss/**/*.scss', gulp / parallel('sass'))
-    gulp.watch('WxlletUP/*.html', gulp/parallel('html'))
-    gulp.watch('WxlletUP/js/*.js', gulp/parallel('js'))
+  gulp.watch('WxlletUP/scss/**/*.scss', gulp.parallel('sass'))
+  gulp.watch('WxlletUP/*.html', gulp.parallel('html'))
+  gulp.watch('WxlletUP/js/*.js', gulp.parallel('js'))
 });
+
+gulp.task('build', gulp.series('clean', 'export'));
+
+gulp.task('default', gulp.parallel('style', 'script', 'sass', 'watch', 'browser-sync'))
